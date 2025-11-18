840351469686184969a284c9db4600a1fad1c4ff
KimSnowFlower <kimbona1148@gmail.com>
2025-11-19 02:45:22 +0900

    feat: #23 add group and groupTodo controller with CRUD Handling

---
+++ src/controllers/group.controller.js
@@ -0,0 +1,62 @@
+const groupRepo = require('../repositories/group.repository');
+
+async function getGroups(req, res) {
+    const userId = req.user.id;
+    const groups = await groupRepo.findGroupsByUser(userId);
+
+    res.json(groups);
+}
+
+async function createGroup(req, res) {
+    const userId = req.user.id;
+    const { code, name } = req.body;
+
+    const groupId = await groupRepo.createGruop({ code, name, creatorId: userId });
+
+    await groupRepo.addMember(groupId, userId);
+
+    res.status(201).json({ success: true, groupId });
+}
+
+async function checkGroupCode(res, req) {
+    const { code } = req.parms;
+    const exists = await groupRepo.checkGroupCodeExists(code);
+
+    res.json({ exists });
+}
+
+async function joinGroup(req, res) {
+    const userId = req.user.id;
+    const { groupId: joinGroupCode } = req.body;
+
+    const group = await groupRepo.findGroupByCode(joinGroupCode);
+
+    if (!group)
+        return res.status(401).json({ error: '그룹을 찾을 수 없습니다.' });
+
+    await groupRepo.addMember(group.id, userId);
+
+    res.status(201).json({ success: true, groupdId: group.id });
+}
+
+async function leaveGroup(req, res) {
+    const userId = req.user.id;
+    const { groupId } = req.body;
+
+    const result = await groupRepo.removeMemberByCode(groupdId, userId);
+
+    if (result.affectedRows === 0)
+        return res
+            .status(404)
+            .json({ error: '그룹에서 탈퇴할 수 없습니다. 사용자 정보를 확인해주세요.' });
+
+    res.status(200).json({ success: true });
+}
+
+module.exports = {
+    getGroups,
+    createGroup,
+    checkGroupCode,
+    joinGroup,
+    leaveGroup,
+};
---
+++ src/controllers/groupTodo.conttroller.js
@@ -0,0 +1,43 @@
+const groupTodoRepo = require('../repositories/groupTodo.repository');
+
+async function getGroupTodos(req, res) {
+    const groupId = req.query.groupId;
+    const gTodo = await groupTodoRepo.findByGroup(groupId);
+
+    res.status(200).json({ gTodo });
+}
+
+async function createGroupTodo(req, res) {
+    const userId = req.user.id;
+    const { groupId, content, completed } = req.body;
+
+    const newTodo = await groupTodoRepo.createGroupTodo({
+        groupId, userId, content, completed,
+    });
+
+    res.status(201).json({ newTodo });
+}
+
+async function deleteGroupTodo(req, res) {
+    const { id } = req.parms;
+
+    await groupTodoRepo.deleteGroupTodo(id);
+
+    res.status(200).json({ message: 'Group Todo item deleted successfully' });
+}
+
+async function patchGroupTodo(req, res) {
+    const { id } = req.parms;
+    const { completed } = req.body;
+
+    await groupTodoRepo.updateCompleted(id, completed);
+
+    res.status(200).json({ message: 'Group Todo item checked update successfully' });
+}
+
+module.exports = {
+    getGroupTodos,
+    createGroupTodo,
+    deleteGroupTodo,
+    patchGroupTodo,
+};
