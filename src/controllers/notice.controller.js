5b24ee3e7315d4143124f1fc6ad652d471b4f6fc
KimSnowFlower <kimbona1148@gmail.com>
2025-11-19 02:55:22 +0900

    feat: #24 add Notice controller with CRUD Handling

---
+++ src/controllers/notice.controller.js
@@ -0,0 +1,47 @@
+const noticeRepo = require('../repositories/notice.repository');
+const userRepo = require('../repositories/user.repository');
+
+async function getNotices(req, res) {
+    const groupId = req.query.groupId;
+    const notices = await noticeRepo.findByGroup(groupId);
+
+    res.status(200).json({ notices });
+}
+
+async function createNotice(req, res) {
+    const userId = req.user.id;
+    const { groupId, title, content } = req.body;
+
+    const user = await userRepo.findById(userId);
+    const author = user?.name || 'Unknown';
+
+    const newNotice = await noticeRepo.createNotice({
+        userId, groupId, title, content, author,
+    });
+
+    res.status(200).json({ newNotice });
+}
+
+async function deleteNotice(req, res) {
+    const { noticeId } = req.parms;
+
+    await noticeRepo.deleteNotice(noticeId);
+
+    res.status(200).json({ message: 'Notice item deleted successfully' });
+}
+
+async function patchNotice(req, res) {
+    const { noticeId } = req.parms;
+    const { title, content, groupId } = req.body;
+
+    await noticeRepo.updateNotice({ id: noticeId, groupId, title, content });
+
+    res.status(200).json({ message: 'Notice item updated successfully' });
+}
+
+module.exports = {
+    getNotices,
+    createNotice,
+    deleteNotice,
+    patchNotice,
+};
